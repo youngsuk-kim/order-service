972deb1a5c10cdc35b3b464651cda9cdc2294bd5
stayforyou <bread.s@kakao.com>
2024-05-03 17:27:03 +0900

    TEST: 관리자는 주문 금액을 산출한다

--- src/test/kotlin/com/example/order/OrderSteps.kt
+++ src/test/kotlin/com/example/order/OrderSteps.kt
@@ -12,6 +12,8 @@ class Order(private val orderItems: List<OrderItem>) {
     fun totalQuantity() = this.orderItems.sumOf { orderItem -> orderItem.quantity }
 
     fun retrieveQuantity(id: Long) = this.orderItems.first { id == it.id }.quantity
+
+    fun charge() = this.orderItems.sumOf { orderItem -> orderItem.price }
 }
 
 class AuthService(
@@ -47,6 +49,7 @@ class OrderItem(
     val productName: String,
     val productPrice: BigDecimal,
     val quantity: Long,
+    val price: BigDecimal,
 )
 
 data class PhoneNumber(val number: String) {
@@ -85,115 +88,153 @@ class DeliveryFakeApi : DeliveryApi {
     }
 }
 
-class OrderSteps : FeatureSpec({
+class OrderSteps : FeatureSpec(
+    {
 
-    feature("주문 준비 스텝") {
-        scenario("점원이 관리자에게 주문을 요청한다") {
-            // Given
-            val orderItems =
-                listOf(
-                    OrderItem(1L, "조던 덩크 하이", BigDecimal.valueOf(10_000), quantity = 3),
-                    OrderItem(1L, "아디다스 루이비통 스니커즈", BigDecimal.valueOf(24_000), quantity = 1),
-                )
+        feature("주문 준비 스텝") {
+            scenario("점원이 관리자에게 주문을 요청한다") {
+                // Given
+                val orderItems =
+                    listOf(
+                        OrderItem(
+                            1L,
+                            "조던 덩크 하이",
+                            BigDecimal.valueOf(10_000),
+                            quantity = 3,
+                            price = BigDecimal.valueOf(100_000),
+                        ),
+                        OrderItem(
+                            1L,
+                            "아디다스 루이비통 스니커즈",
+                            BigDecimal.valueOf(24_000),
+                            quantity = 1,
+                            price = BigDecimal.valueOf(202_000),
+                        ),
+                    )
 
-            // When
-            val requestOrder = Order.request(orderItems)
+                // When
+                val sut = Order.request(orderItems)
 
-            // Then
-            requestOrder.totalQuantity() shouldBe 4
+                // Then
+                sut.totalQuantity() shouldBe 4
+            }
+
+            scenario("관리자는 토큰 정보를 통해 손님의 신원을 파악한다") {
+                // Given
+                val token = "user-token"
+
+                // When
+                val sut = AuthService(AuthFakeApi()).getUserId(token)
+
+                // Then
+                sut shouldBe 1L
+            }
+
+            scenario("관리자는 주문한 물품의 재고가 충분한지 확인한다") {
+                // Given
+                val itemId = 1L
+
+                // When
+                val sut = ProductService(ProductFakeApi()).isProductQuantityEnough(itemId)
+
+                // Then
+                sut shouldBe true
+            }
+
+            scenario("관리자는 손님의 핸드폰 번호의 유효성을 검사한다") {
+                // Given When
+                val sut = PhoneNumber("01030202322")
+
+                // Then
+                sut.number shouldBe "01030202322"
+            }
+
+            scenario("관리자는 손님의 배송지가 도서 산간 지역인지 확인한다") {
+                // Given
+                val postNum = "363"
+
+                // When
+                val sut = DeliveryService(DeliveryFakeApi()).isSurChargeArea(postNum)
+
+                // Then
+                sut shouldBe true
+            }
+
+            scenario("관리자는 주문 금액을 산출한다") {
+                val orderItems =
+                    listOf(
+                        OrderItem(
+                            1L,
+                            "조던 덩크 하이",
+                            BigDecimal.valueOf(10_000),
+                            quantity = 3,
+                            price = BigDecimal.valueOf(100_000),
+                        ),
+                        OrderItem(
+                            1L,
+                            "아디다스 루이비통 스니커즈",
+                            BigDecimal.valueOf(24_000),
+                            quantity = 1,
+                            price = BigDecimal.valueOf(202_000),
+                        ),
+                    )
+
+                val requestOrder = Order.request(orderItems)
+
+                // When
+                val sut = requestOrder.charge()
+
+                // Then
+                sut shouldBe BigDecimal(302_000)
+            }
         }
 
-        scenario("관리자는 토큰 정보를 통해 손님의 신원을 파악한다") {
-            // Given
-            val token = "user-token"
+        feature("결제 스텝") {
+            scenario("관리자는 결제 직원에게 손님 정보, 주문 내역 정보, 카드 정보, 결제 금액을 건네준다") {
+                // 결제 직원에게 정보 전달 확인
+            }
 
-            // When
-            val userId = AuthService(AuthFakeApi()).getUserId(token)
+            scenario("결제 직원은 결제를 완료했다고 통지한다") {
+                // 결제 완료 여부 확인
+            }
 
-            // Then
-            userId shouldBe 1L
+            scenario("관리자는 결제가 실패했을 경우를 대비한다") {
+                // 결제 실패에 대한 대비
+            }
         }
 
-        scenario("관리자는 주문한 물품의 재고가 충분한지 확인한다") {
-            // Given
-            val itemId = 1L
+        feature("주문 완료 스텝") {
+            scenario("시간 제한 주문이라면, 점원에게 주문 시간이 초과되었다고 알린다") {
+                // 시간 초과에 대한 알림 로직
+            }
 
-            // When
-            val quantityEnough = ProductService(ProductFakeApi()).isProductQuantityEnough(itemId)
+            scenario("관리자는 결제 정보와 주문 정보가 일치하는지 확인한다") {
+                // 정보 일치 확인
+            }
 
-            // Then
-            quantityEnough shouldBe true
+            scenario("관리자는 재고 관리실에 아무도 못 들어오게 막는다") {
+                // 재고 관리 제한
+            }
+
+            scenario("관리자는 재고를 감소시킨다") {
+                // 재고 감소 로직 추가
+            }
+
+            scenario("관리자는 결제 정보를 통해 주문을 완료로 표시한다") {
+                // 주문 완료 표시
+            }
+
+            scenario("택배 배송을 선택한 고객이라면, 배송 예정 표에 주문을 추가한다") {
+                // 배송 예정 추가
+            }
+
+            scenario("고객이 선택한 배송 방식에 따라 배송을 준비한다") {
+                // 배송 방식에 따른 배송 준비
+            }
+
+            scenario("고객에게 주문 완료 및 배송 준비 상황을 알린다") {
+                // 주문 완료 및 배송 정보 알림
+            }
         }
-
-        scenario("관리자는 손님의 핸드폰 번호의 유효성을 검사한다") {
-            // Given When
-            val phoneNumber = PhoneNumber("01030202322")
-
-            // Then
-            phoneNumber.number shouldBe "01030202322"
-        }
-
-        scenario("관리자는 손님의 배송지가 도서 산간 지역인지 확인한다") {
-            // Given
-            val postNum = "363"
-
-            // When
-            val sut = DeliveryService(DeliveryFakeApi()).isSurChargeArea(postNum)
-
-            // Then
-            sut shouldBe true
-        }
-
-        scenario("관리자는 주문 금액을 산출한다") {
-            // 주문 총액 산출
-        }
-    }
-
-    feature("결제 스텝") {
-        scenario("관리자는 결제 직원에게 손님 정보, 주문 내역 정보, 카드 정보, 결제 금액을 건네준다") {
-            // 결제 직원에게 정보 전달 확인
-        }
-
-        scenario("결제 직원은 결제를 완료했다고 통지한다") {
-            // 결제 완료 여부 확인
-        }
-
-        scenario("관리자는 결제가 실패했을 경우를 대비한다") {
-            // 결제 실패에 대한 대비
-        }
-    }
-
-    feature("주문 완료 스텝") {
-        scenario("시간 제한 주문이라면, 점원에게 주문 시간이 초과되었다고 알린다") {
-            // 시간 초과에 대한 알림 로직
-        }
-
-        scenario("관리자는 결제 정보와 주문 정보가 일치하는지 확인한다") {
-            // 정보 일치 확인
-        }
-
-        scenario("관리자는 재고 관리실에 아무도 못 들어오게 막는다") {
-            // 재고 관리 제한
-        }
-
-        scenario("관리자는 재고를 감소시킨다") {
-            // 재고 감소 로직 추가
-        }
-
-        scenario("관리자는 결제 정보를 통해 주문을 완료로 표시한다") {
-            // 주문 완료 표시
-        }
-
-        scenario("택배 배송을 선택한 고객이라면, 배송 예정 표에 주문을 추가한다") {
-            // 배송 예정 추가
-        }
-
-        scenario("고객이 선택한 배송 방식에 따라 배송을 준비한다") {
-            // 배송 방식에 따른 배송 준비
-        }
-
-        scenario("고객에게 주문 완료 및 배송 준비 상황을 알린다") {
-            // 주문 완료 및 배송 정보 알림
-        }
-    }
-})
+    },
+)
